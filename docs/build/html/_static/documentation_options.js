000604998ea903e1479f792e5642572fbe0d26f9
alexOarga <alex718123@gmail.com>
2021-04-13 00:14:48 +0200

    Add facadeUtils docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2bee740',
+    VERSION: 'eb7c87e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
