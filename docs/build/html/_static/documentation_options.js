1f1bd2f2b1e5c985db9d5f2439c19859e31bf21d
alexOarga <alex718123@gmail.com>
2021-06-23 21:39:04 +0200

    fix depedencies

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '513cb54',
+    VERSION: '6b6b58e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
